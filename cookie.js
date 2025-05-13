2c2cf8a8e297ba69a0be076ebbbe18c14da6fce2
lingus955 <lingus955@edu.linkoping.se>
2025-05-13 09:01:59 +0000

    sync

--- cookie.js
+++ cookie.js
@@ -2,12 +2,17 @@ const cookiebar = document.querySelector(".cookie-bar");
 const cookieBtn = document.querySelector("#button-yes");
 
 cookieBtn.addEventListener("click", () => {
-  alert("Hej hej du klickade på knappen!");
+  alert("du klickade på knappen!");
 });
 
 
 cookieBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  cookiebar.classList.remove("cookie-banner-active");
-  localStorage.setItem("cookieBannerDisplayed", "true");
+  cookiebar.classList.remove("cookieparent");
+  localStorage.setItem("cookieBarDisplayed", "true");
 });
+
+if(localStorage.getItem("cookieBannerDisplayed")){
+  cookieBanner.classList.remove("cookieparent");
+}
+
